17f2f000acf0c0a7b00259f3384fb2c6eb962b9a
mariela1982 <marimanri_24@hotmail.com>
2023-09-19 08:57:11 -0300

    materia

--- src/ar/edu/unlam/pb2/integrador1/Materia.java
+++ src/ar/edu/unlam/pb2/integrador1/Materia.java
@@ -1,5 +1,29 @@
 package ar.edu.unlam.pb2.integrador1;
 
 public class Materia {
+	private Integer codigo;
+	private String nombre;
+
+	public Materia(Integer codigo, String nombre) {
+	this.codigo =codigo;
+	this.nombre=nombre;
+	}
+
+	public Integer getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Integer codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
 
 }
